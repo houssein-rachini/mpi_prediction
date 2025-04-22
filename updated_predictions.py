81c6eaef6fdd12b5522cb655340ecbb83866ecad
Hussein Al Rachini <ha333@aub.edu.lb>
2025-04-22 16:49:54 +0300

    testing sev pov changes

--- updated_predictions.py
+++ updated_predictions.py
@@ -3567,7 +3567,8 @@ def show_helper_tab(df_actual):
                 & (df_actual["Region"] == d["Region"])
                 & (df_actual["Year"] == d["Year"])
             ]["MPI"]
-
+            if display_sev_pov:
+                show_actual = False
             if show_actual:
                 if actual_val.empty:
                     continue  # Skip if no actual MPI and showing actual
@@ -3583,7 +3584,6 @@ def show_helper_tab(df_actual):
                 5,
             )
             if display_sev_pov:
-                show_actual = False
                 value = pred_pov
             geojson_features.append(
                 {
