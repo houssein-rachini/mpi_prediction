2d9bacdea1604d46114d402b3b23c06ed65afd1d
Hussein Al Rachini <ha333@aub.edu.lb>
2026-01-30 14:21:34 +0200

    init errors fix

--- visualization.py
+++ visualization.py
@@ -10,7 +10,16 @@ import branca.colormap as cm
 import pandas as pd
 
 
-ee.Initialize()
+from google.oauth2 import service_account
+
+service_account_info = dict(st.secrets["google_ee"])  # No need for .to_json()
+
+credentials = service_account.Credentials.from_service_account_info(
+    service_account_info, scopes=["https://www.googleapis.com/auth/earthengine"]
+)
+
+ee.Initialize(credentials)
+
 # Load FAO GAUL dataset
 fao_gaul = ee.FeatureCollection("FAO/GAUL_SIMPLIFIED_500m/2015/level1")
 
